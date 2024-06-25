fe4d4451ee4eb88a7a122937a3fbe81dc323455a
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-25 19:14:11 +0200

    Modified

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -4,14 +4,19 @@ package se.lexicon;
 public class App {
     public static void main(String[] args) {
 
-        BankAccount customer1 = new BankAccount();
-         customer1.name = "John Baptist";
-         customer1.email = "bapti@google.com";
-         customer1.phoneNumber = "07011112365";
-         customer1.accountNumber = "782225456521";
+       /* BankAccount customer = new BankAccount();
+         customer.name = "John Baptist";
+         customer.email = "bapti@google.com";
+         customer.phoneNumber = "07011112365";
+         customer.accountNumber = "782225456521";
+         customer.balance = 2000;
+         
+        System.out.println(customer.depositFund());
+        System.out.println(customer.withdrawFund());*/
 
-
-
+        String name;
+        BankAccount customer1 = new BankAccount( "john Baptist","johb@google.com", "07025686145", "05236545",  2000);
+        BankAccount customer2 = new BankAccount( "David lund","david@google.com", "07025673648", "05236855",  4000);
 
     }
 }
