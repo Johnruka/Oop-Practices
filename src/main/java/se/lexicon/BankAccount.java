76b5b587d876ef4f76d099cbd163536686daa6a6
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-06-25 19:13:47 +0200

    Modified

--- src/main/java/se/lexicon/BankAccount.java
+++ src/main/java/se/lexicon/BankAccount.java
@@ -7,4 +7,29 @@ public class BankAccount {
     String phoneNumber;
     String accountNumber;
     int balance;
+
+    public BankAccount(String name, String email, String phoneNumber, String accountNumber, int balance) {
+        name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+
+    public String depositFund() {
+        String result = "depositedAmount";
+        return result;
+    }
+
+    public boolean withdrawFund() {
+        String result = "withdrawnAmount";
+        int withdrawnAmount = 0;
+
+
+        if (withdrawnAmount == 0) {
+            return false;
+        }
+        return false;
+    }
 }
